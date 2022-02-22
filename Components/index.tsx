e764fdfa190d12a35eeabc475e79d6b39fa431f0
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-02-22 03:55:36 +0200

    created modular export file to modularize and clean up component exports onto their respective pages

---
+++ Components/index.tsx
@@ -0,0 +1,6 @@
+export { Nav } from './Nav';
+export { Footer } from './Footer';
+export { PhotoCard } from './PhotoCard';
+export { Loader } from './Loader';
+export { DescriptionCard } from './DescriptionCard';
+export { LoadingDots } from './LoadingDots';
