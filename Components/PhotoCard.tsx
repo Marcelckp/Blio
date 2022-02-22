ab6dfcbd1e5dca59f92f333a3e97ec407123263c
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-02-22 03:56:53 +0200

    created photo mobile responsive page photo card component to display stories/ image cards

---
+++ Components/PhotoCard.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import React from 'react';
+import { whiteArrow } from '../public/assets/shared/desktop/arrow';
+
+
+interface Props {
+    creator: string;
+    postId: number;
+    createdAt: number;
+    title: string;
+    src: string
+}
+
+export const PhotoCard = ({creator, postId, title, createdAt, src}: Props) => {
+
+  return (
+      <div className={`cursor-pointer hover:-translate-y-10 transition-all duration-300 ease-in-out relative w-full  flex items-center justify-center h-[600px] bg-no-repeat bg-cover object-fill`}>
+          <Image quality={100} className='-z-10' src={src} layout='fill' objectFit='cover' />
+          <div className='w-full h-full flex justify-center items-end bg-gradient-to-b from-[#fff0] to-[#00000080]'>
+            <div className='w-10/12 py-5 text-white'>
+                <div className=' border-b-[1px] border-gray-500 py-5'>
+                    <p className='text-sm py-1'>{ createdAt }</p>
+                    <h1 className='font-semibold text-2xl'>{ title}</h1>
+                    <p className='text-sm py-1'>by { creator }</p>
+                </div>
+                <div className='w-full flex justify-between items-center py-4' onClick={() => {}}>
+                    <h2 className='tracking-widest text-base'>READ STORY</h2>
+                    { whiteArrow }
+                </div>
+            </div>
+          </div>
+      </div>
+  );
+};
