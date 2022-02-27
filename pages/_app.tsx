fe60251f80b27bd0d27ab3d669de75487199c856
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-02-27 22:43:58 +0200

    wrapped app component to conditionally render a redirect component to take logged in users to the post creation page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,16 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 import { store } from '../redux/store';
-import { Footer, Nav } from '../Components';
+import { CreatePostIcon, Footer, Nav } from '../Components';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const user = store.getState().user.user
   return (
     <Provider store={store}>
       <Nav />
       <Component  {...pageProps} />
+      { user && <CreatePostIcon />}
       <Footer />
     </Provider>
   )
