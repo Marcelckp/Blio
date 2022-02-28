562eaa51363dbf7da50cce2331df95c35cf829f6
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-02-28 23:08:48 +0200

    updated next js configuration file

--- next.config.js
+++ next.config.js
@@ -4,4 +4,12 @@ module.exports = {
   images: {
     domains: ['iili.io'],
   },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/post/:path*',
+  //       destination: 'http://freeimage.host/api/1/upload/?key6d207e02198a847aa98d0a2a901485a5=source=C:\fakepath\A4B30D0A-EB5C-4617-8EE2-5E4C6220AAA6.heic&format=json'
+  //     }
+  //   ]
+  // }
 }
