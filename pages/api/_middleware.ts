4faffb21bd69bc7aa779b66b368bcc38da7d83ae
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-03-01 06:52:39 +0200

    update

--- pages/api/_middleware.ts
+++ pages/api/_middleware.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { NextResponse } from 'next/server'
 
-export default function middleware(req: NextApiRequest, res: NextApiResponse) {
+export default async function middleware(req: NextApiRequest, res: NextApiResponse) {
     return NextResponse.next()
     // NextResponse.redirect('/login)
 }
