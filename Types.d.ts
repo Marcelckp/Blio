56945f53ea36520d12c1a2b95bfd1c0ed1b76f33
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-03-06 21:52:20 +0200

    new export type added

--- Types.d.ts
+++ Types.d.ts
@@ -8,3 +8,18 @@ export interface Post {
   user_id: number
   created_on: string
 }
+
+export interface InnerJoinPost {
+  photo: string
+  body: string
+  title: string
+  body: string
+  blur: string
+  post_id: number
+  user_id: number
+  created_on: string
+  fullname: string
+  username: string
+  last_login: string
+  password: string
+}
