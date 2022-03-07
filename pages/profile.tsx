0c4322aae2e8fb32408286c795363d6d1a641656
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-03-07 02:56:44 +0200

    added functionality to show the logged in users created photo blog posts using SSR

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import { Nav } from '../Components'
+import axios from 'axios'
+import Cookies from 'js-cookie'
+import { GetServerSideProps } from 'next'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { GetStateFromSelectors } from 'reselect/es/types'
+import { store } from '../redux/store'
+import { InnerJoinPost } from '../Types'
 
-export default function profile() {
+interface Props {
+  data: [InnerJoinPost]
+}
+
+export default function profile({ data }: Props) {
+  const user = useSelector((state: any) => state.user.user)
+  // console.log(data)
+  useEffect(() => {
+    if (!user) window.location.replace('/signin')
+  })
   return (
     <div>
-        <h1>Profile</h1>
+      <h1>Profile</h1>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  // console.log(req.cookies);
+
+  let data: any = []
+  try {
+    data = await axios.post<Array<InnerJoinPost>>(
+      `http://localhost:3000/api/post/read/specific`,
+      { data: { cookie: req.cookies } }
+    )
+  } catch (e) {
+    console.log(e);
+  }
+
+  return {
+    props: {
+      data: data!.data?.posts,
+    },
+  }
 }
