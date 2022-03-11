82b7d8af344e91e6ef0f19b07458a73321acb876
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-03-11 04:58:09 +0200

    set SSR to pre fetch the data & individual photos for a specific user on the application

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -6,6 +6,8 @@ import { useSelector } from 'react-redux'
 import { GetStateFromSelectors } from 'reselect/es/types'
 import { store } from '../redux/store'
 import { InnerJoinPost } from '../Types'
+import { UserCircleIcon } from '@heroicons/react/solid'
+import { PhotoCard } from '../Components'
 
 interface Props {
   data: [InnerJoinPost]
@@ -13,13 +15,38 @@ interface Props {
 
 export default function profile({ data }: Props) {
   const user = useSelector((state: any) => state.user.user)
-  // console.log(data)
+  console.log(data, user)
   useEffect(() => {
     if (!user) window.location.replace('/signin')
   })
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className="flex w-full flex-col min-h-screen items-center">
+      <section className="flex w-full relative max-w-screen-2xl flex-col items-center justify-center">
+        <div className="relative top-0 flex h-96 w-full items-center justify-center bg-gradient-to-r from-[#FFC593] via-[#BC7198] to-[#5A77FF]">
+          <section className=" mt-16 flex h-32 w-10/12 justify-between rounded bg-white p-10">
+            <div className="flex items-center space-x-5">
+              <div>
+                {user && user.profile_photo ? (
+                  <img src={user.profile_picture} alt="" />
+                ) : (
+                  <UserCircleIcon className="w-22 h-20" />
+                )}
+              </div>
+              <div className="flex flex-col">
+                <h1>{user && user.fullname}</h1>
+                <h1>{user && user.email}</h1>
+              </div>
+            </div>
+            <div>
+              <h1>metrics</h1>
+            </div>
+          </section>
+        </div>
+        <div className="relative w-full md:grid md:grid-cols-2  xl:grid-cols-4">
+          {data && data.map((item: InnerJoinPost, index) => <PhotoCard creator={item.fullname} postId={item.post_id} src={item.photo} title={item.title} createdAt={item.created_on}  />
+          )}
+        </div>
+      </section>
     </div>
   )
 }
@@ -34,7 +61,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
       { data: { cookie: req.cookies } }
     )
   } catch (e) {
-    console.log(e);
+    console.log(e)
   }
 
   return {
