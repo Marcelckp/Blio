3e32668fa5ab5e1162b8a7fad31957db1aacd47f
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-03-20 04:43:30 +0200

    created component to show when a viewed user profile has no posts on the account

--- Components/NoPostsIcon.tsx
+++ Components/NoPostsIcon.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useRouter } from 'next/router'
 
 export const NoPostsIcon = () => {
@@ -8,7 +7,7 @@ const router = useRouter()
       <div className='w-1/2 text-center'>
         <h1 className="text-2xl font-semibold mb-5">You have not created any posts as yet.</h1>
         <p className="mb-5">To create a post click here</p>
-        <button className='cursor-pointer bg-black px-3 py-2 font-medium tracking-widest text-white transition duration-300 ease-in-out hover:bg-opacity-40'>CREATE POST</button>
+        <button onClick={() => router.push('/create')} className='cursor-pointer bg-black px-3 py-2 font-medium tracking-widest text-white transition duration-300 ease-in-out hover:bg-opacity-40'>CREATE POST</button>
       </div>
     </div>
   )
